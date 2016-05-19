c4f2f7e487687041a12d10925893c8b162acdde3
Sebastian <sebastian@ghetu.net>
2016-05-19 23:26:23 +0100

    Switched settings Activity to AppCompat DayNight theme

--- src/com/vizdashcam/activities/ActivitySettings.java
+++ src/com/vizdashcam/activities/ActivitySettings.java
@@ -2,23 +2,47 @@ package com.vizdashcam.activities;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.vizdashcam.R;
 import com.vizdashcam.fragments.FragmentPreferences;
 
-public class ActivitySettings extends PreferenceActivity {
+import java.util.List;
 
-	@SuppressWarnings("unused")
-	private static final String TAG = "Settings";
+public class ActivitySettings extends AppCompatPreferenceActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    private static final String TAG = "Settings";
 
-		getActionBar().setTitle("Settings");
+    private Toolbar toolbar;
 
-		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, new FragmentPreferences())
-				.commit();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
 
+        initViews();
+        findViews();
+
+        // Android API looks through rootView for @android:id/list and replaces that with the
+        // FragmentPreferences instance
+        getFragmentManager().beginTransaction()
+                .replace(R.id.rootView, new FragmentPreferences()).commit();
+    }
+
+    private void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    private void findViews() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 }
--- src/com/vizdashcam/fragments/FragmentPreferences.java
+++ src/com/vizdashcam/fragments/FragmentPreferences.java
@@ -16,223 +16,219 @@ import com.vizdashcam.utils.FeedbackSoundPlayer;
 
 public class FragmentPreferences extends PreferenceFragment {
 
-	public static final String TAG = "FragmentPreferences";
+    public static final String TAG = "FragmentPreferences";
 
-	private static final String KEY_LP_VIDEO_QUALITY = "defaultCamcorderProfile";
-	private static final String KEY_LP_VIDEO_LENGTH = "defaultVideoLength";
-	private static final String KEY_CP_LOOP_ACTIVE = "loopModeActive";
-	private static final String KEY_CP_SHOCK_ACTIVE = "shockModeActive";
-	private static final String KEY_LP_SHOCK_SENSITIVITY = "defaultShockSensitivity";
-	private static final String KEY_CP_LONG_PRESS = "longPressToMarkActive";
-	private static final String KEY_CP_AUDIO_FEEDBACK_SHOCK = "audioFeedbackShockActive";
-	private static final String KEY_CP_AUDIO_FEEDBACK_BUTTON = "audioFeedbackButtonActive";
-	private static final String KEY_CP_TACTILE_FEEDBACK = "tactileFeedbackActive";
-	private static final String KEY_CP_SPEEDOMETER_ACTIVE = "speedometerActive";
-	private static final String KEY_LP_SPEEDOMETER_UNITS = "speedometerUnitsMeasure";
+    private static final String KEY_LP_VIDEO_QUALITY = "defaultCamcorderProfile";
+    private static final String KEY_LP_VIDEO_LENGTH = "defaultVideoLength";
+    private static final String KEY_CP_LOOP_ACTIVE = "loopModeActive";
+    private static final String KEY_CP_SHOCK_ACTIVE = "shockModeActive";
+    private static final String KEY_LP_SHOCK_SENSITIVITY = "defaultShockSensitivity";
+    private static final String KEY_CP_LONG_PRESS = "longPressToMarkActive";
+    private static final String KEY_CP_AUDIO_FEEDBACK_SHOCK = "audioFeedbackShockActive";
+    private static final String KEY_CP_AUDIO_FEEDBACK_BUTTON = "audioFeedbackButtonActive";
+    private static final String KEY_CP_TACTILE_FEEDBACK = "tactileFeedbackActive";
+    private static final String KEY_CP_SPEEDOMETER_ACTIVE = "speedometerActive";
+    private static final String KEY_LP_SPEEDOMETER_UNITS = "speedometerUnitsMeasure";
 
-	GlobalState mAppState;
+    GlobalState mAppState;
 
-	@SuppressWarnings("unused")
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		FeedbackClickListener feedbackClickListener = new FeedbackClickListener();
-		FeedbackPreferenceChangeListener feedbackPreferenceChangeListener = new FeedbackPreferenceChangeListener();
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preference_fragment);
+    }
 
-		mAppState = (GlobalState) getActivity().getApplicationContext();
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-		ListPreference videoQualityList = (ListPreference) findPreference(KEY_LP_VIDEO_QUALITY);
-		ListPreference videoLengthList = (ListPreference) findPreference(KEY_LP_VIDEO_LENGTH);
-		final CheckBoxPreference useLoopMode = (CheckBoxPreference) findPreference(KEY_CP_LOOP_ACTIVE);
-		final CheckBoxPreference shockModeActive = (CheckBoxPreference) findPreference(KEY_CP_SHOCK_ACTIVE);
-		final ListPreference shockSensitivity = (ListPreference) findPreference(KEY_LP_SHOCK_SENSITIVITY);
-		final CheckBoxPreference longPressToMarkActive = (CheckBoxPreference) findPreference(KEY_CP_LONG_PRESS);
-		final CheckBoxPreference audioFeedbackButtonActive = (CheckBoxPreference) findPreference(KEY_CP_AUDIO_FEEDBACK_BUTTON);
-		final CheckBoxPreference audioFeedbackShockActive = (CheckBoxPreference) findPreference(KEY_CP_AUDIO_FEEDBACK_SHOCK);
-		final CheckBoxPreference tactileFeedbackActive = (CheckBoxPreference) findPreference(KEY_CP_TACTILE_FEEDBACK);
-		final CheckBoxPreference speedometerActive = (CheckBoxPreference) findPreference(KEY_CP_SPEEDOMETER_ACTIVE);
-		final ListPreference speedometerUnitsMeasure = (ListPreference) findPreference(KEY_LP_SPEEDOMETER_UNITS);
+        FeedbackClickListener feedbackClickListener = new FeedbackClickListener();
+        FeedbackPreferenceChangeListener feedbackPreferenceChangeListener = new FeedbackPreferenceChangeListener();
 
-		// Video Quality List
-		videoQualityList.setEntries(mAppState.getSupportedVideoQualities());
-		videoQualityList.setEntryValues(mAppState
-				.getSupportedVideoQualitiesIntegers());
-		if (videoQualityList.getValue() == null) {
-			videoQualityList.setValueIndex(0);
-		}
-		if (mAppState.isRecording()) {
-			videoQualityList.setEnabled(false);
-			videoQualityList
-					.setSummary("Set video quality. Please stop recording before changing this.");
-		} else {
-			videoQualityList.setEnabled(true);
-			videoQualityList.setSummary("Set video quality");
-		}
-		videoQualityList.setOnPreferenceClickListener(feedbackClickListener);
-		videoQualityList
-				.setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
+        mAppState = (GlobalState) getActivity().getApplicationContext();
 
-		// Video Length List
-		if (videoLengthList.getValue() == null) {
-			videoLengthList.setValueIndex(2);
-		}
-		if (mAppState.isRecording()) {
-			videoLengthList.setEnabled(false);
-			videoLengthList
-					.setSummary("Set video length. Please stop recording before changing this.");
-		} else {
-			videoLengthList.setEnabled(true);
-			videoLengthList.setSummary("Set video length");
-		}
-		videoLengthList.setOnPreferenceClickListener(feedbackClickListener);
-		videoLengthList
-				.setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
+        ListPreference videoQualityList = (ListPreference) findPreference(KEY_LP_VIDEO_QUALITY);
+        ListPreference videoLengthList = (ListPreference) findPreference(KEY_LP_VIDEO_LENGTH);
+        final CheckBoxPreference useLoopMode = (CheckBoxPreference) findPreference(KEY_CP_LOOP_ACTIVE);
+        final CheckBoxPreference shockModeActive = (CheckBoxPreference) findPreference(KEY_CP_SHOCK_ACTIVE);
+        final ListPreference shockSensitivity = (ListPreference) findPreference(KEY_LP_SHOCK_SENSITIVITY);
+        final CheckBoxPreference longPressToMarkActive = (CheckBoxPreference) findPreference(KEY_CP_LONG_PRESS);
+        final CheckBoxPreference audioFeedbackButtonActive = (CheckBoxPreference) findPreference(KEY_CP_AUDIO_FEEDBACK_BUTTON);
+        final CheckBoxPreference audioFeedbackShockActive = (CheckBoxPreference) findPreference(KEY_CP_AUDIO_FEEDBACK_SHOCK);
+        final CheckBoxPreference tactileFeedbackActive = (CheckBoxPreference) findPreference(KEY_CP_TACTILE_FEEDBACK);
+        final CheckBoxPreference speedometerActive = (CheckBoxPreference) findPreference(KEY_CP_SPEEDOMETER_ACTIVE);
+        final ListPreference speedometerUnitsMeasure = (ListPreference) findPreference(KEY_LP_SPEEDOMETER_UNITS);
 
-		// Loop Mode
-		useLoopMode.setDefaultValue(false);
-		if (mAppState.isRecording()) {
-			useLoopMode.setEnabled(false);
-			useLoopMode
-					.setSummary("Delete old videos when the phone runs out of storage. Please stop recording before changing this.");
-		} else {
-			useLoopMode.setEnabled(true);
-			useLoopMode
-					.setSummary("Delete old videos when the phone runs out of storage");
-		}
-		useLoopMode
-				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+        // Video Quality List
+        videoQualityList.setEntries(mAppState.getSupportedVideoQualities());
+        videoQualityList.setEntryValues(mAppState
+                .getSupportedVideoQualitiesIntegers());
+        if (videoQualityList.getValue() == null) {
+            videoQualityList.setValueIndex(0);
+        }
+        if (mAppState.isRecording()) {
+            videoQualityList.setEnabled(false);
+            videoQualityList
+                    .setSummary("Set video quality. Please stop recording before changing this.");
+        } else {
+            videoQualityList.setEnabled(true);
+            videoQualityList.setSummary("Set video quality");
+        }
+        videoQualityList.setOnPreferenceClickListener(feedbackClickListener);
+        videoQualityList
+                .setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
 
-					@Override
-					public boolean onPreferenceChange(Preference arg0,
-							Object arg1) {
-						audioFeedback();
-						tactileFeedback();
+        // Video Length List
+        if (videoLengthList.getValue() == null) {
+            videoLengthList.setValueIndex(2);
+        }
+        if (mAppState.isRecording()) {
+            videoLengthList.setEnabled(false);
+            videoLengthList
+                    .setSummary("Set video length. Please stop recording before changing this.");
+        } else {
+            videoLengthList.setEnabled(true);
+            videoLengthList.setSummary("Set video length");
+        }
+        videoLengthList.setOnPreferenceClickListener(feedbackClickListener);
+        videoLengthList
+                .setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
 
-						return true;
-					}
-				});
+        // Loop Mode
+        useLoopMode.setDefaultValue(false);
+        if (mAppState.isRecording()) {
+            useLoopMode.setEnabled(false);
+            useLoopMode
+                    .setSummary("Delete old videos when the phone runs out of storage. Please stop recording before changing this.");
+        } else {
+            useLoopMode.setEnabled(true);
+            useLoopMode
+                    .setSummary("Delete old videos when the phone runs out of storage");
+        }
+        useLoopMode
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
-		// Shock Mode
-		shockModeActive.setDefaultValue(false);
-		if (mAppState.isRecording()) {
-			shockModeActive.setEnabled(false);
-			shockModeActive
-					.setSummary("Mark videos during which a shock occured. This prevents LOOP MODE from deleting these videos. Please stop recording before changing this. ");
-		} else {
-			shockModeActive.setEnabled(true);
-			shockModeActive
-					.setSummary("Mark videos during which a shock occured. This prevents LOOP MODE from deleting these videos. ");
-		}
-		shockModeActive
-				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference arg0,
+                                                      Object arg1) {
+                        audioFeedback();
+                        tactileFeedback();
 
-					@Override
-					public boolean onPreferenceChange(Preference preference,
-							Object newValue) {
-						audioFeedback();
-						tactileFeedback();
+                        return true;
+                    }
+                });
 
-						if (shockModeActive.isChecked()) {
-							audioFeedbackShockActive.setEnabled(false);
-							shockSensitivity.setEnabled(false);
-						} else {
-							audioFeedbackShockActive.setEnabled(true);
-							shockSensitivity.setEnabled(true);
-						}
+        // Shock Mode
+        shockModeActive.setDefaultValue(false);
+        if (mAppState.isRecording()) {
+            shockModeActive.setEnabled(false);
+            shockModeActive
+                    .setSummary("Mark videos during which a shock occured. This prevents LOOP MODE from deleting these videos. Please stop recording before changing this. ");
+        } else {
+            shockModeActive.setEnabled(true);
+            shockModeActive
+                    .setSummary("Mark videos during which a shock occured. This prevents LOOP MODE from deleting these videos. ");
+        }
+        shockModeActive
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
-						return true;
-					}
-				});
+                    @Override
+                    public boolean onPreferenceChange(Preference preference,
+                                                      Object newValue) {
+                        audioFeedback();
+                        tactileFeedback();
 
-		// Shock Sensitivity
-		if (shockSensitivity.getValue() == null) {
-			shockSensitivity.setValueIndex(1);
-		}
-		if (mAppState.isRecording()) {
-			shockSensitivity.setEnabled(false);
-			shockSensitivity
-					.setSummary("Set shock detection sensitivity. Please stop recording before changing this.");
-		} else {
-			if (shockModeActive.isChecked()) {
-				shockSensitivity.setEnabled(true);
-			} else {
-				shockSensitivity.setEnabled(false);
-			}
-			shockSensitivity.setSummary("Set shock detection sensitivity");
-		}
-		shockSensitivity.setOnPreferenceClickListener(feedbackClickListener);
-		shockSensitivity
-				.setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
+                        if (shockModeActive.isChecked()) {
+                            audioFeedbackShockActive.setEnabled(false);
+                            shockSensitivity.setEnabled(false);
+                        } else {
+                            audioFeedbackShockActive.setEnabled(true);
+                            shockSensitivity.setEnabled(true);
+                        }
 
-		// Audio Feedback for Shocks
-		audioFeedbackShockActive
-				.setOnPreferenceClickListener(feedbackClickListener);
+                        return true;
+                    }
+                });
 
-		// Audio Feedback for Buttons
-		audioFeedbackButtonActive
-				.setOnPreferenceClickListener(feedbackClickListener);
+        // Shock Sensitivity
+        if (shockSensitivity.getValue() == null) {
+            shockSensitivity.setValueIndex(1);
+        }
+        if (mAppState.isRecording()) {
+            shockSensitivity.setEnabled(false);
+            shockSensitivity
+                    .setSummary("Set shock detection sensitivity. Please stop recording before changing this.");
+        } else {
+            if (shockModeActive.isChecked()) {
+                shockSensitivity.setEnabled(true);
+            } else {
+                shockSensitivity.setEnabled(false);
+            }
+            shockSensitivity.setSummary("Set shock detection sensitivity");
+        }
+        shockSensitivity.setOnPreferenceClickListener(feedbackClickListener);
+        shockSensitivity
+                .setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
 
-		// Tactile Feedback for Buttons
-		tactileFeedbackActive
-				.setOnPreferenceClickListener(feedbackClickListener);
+        // Audio Feedback for Shocks
+        audioFeedbackShockActive
+                .setOnPreferenceClickListener(feedbackClickListener);
 
-		// Speedometer
-		speedometerActive
-				.setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
+        // Audio Feedback for Buttons
+        audioFeedbackButtonActive
+                .setOnPreferenceClickListener(feedbackClickListener);
 
-		// Speedometer Units
-		if (speedometerUnitsMeasure.getValue() == null) {
-			speedometerUnitsMeasure.setValueIndex(1);
-		}
-		speedometerUnitsMeasure
-				.setOnPreferenceClickListener(feedbackClickListener);
-		speedometerUnitsMeasure
-				.setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
-	}
+        // Tactile Feedback for Buttons
+        tactileFeedbackActive
+                .setOnPreferenceClickListener(feedbackClickListener);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        // Speedometer
+        speedometerActive.setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
 
-		// Load the preferences from an XML resource
-		addPreferencesFromResource(R.xml.preference_fragment);
+        // Speedometer Units
+        if (speedometerUnitsMeasure.getValue() == null)
+            speedometerUnitsMeasure.setValueIndex(1);
 
-	}
+        speedometerUnitsMeasure
+                .setOnPreferenceClickListener(feedbackClickListener);
+        speedometerUnitsMeasure
+                .setOnPreferenceChangeListener(feedbackPreferenceChangeListener);
+    }
 
-	private void audioFeedback() {
-		if (mAppState.detectAudioFeedbackButtonActive()) {
-			FeedbackSoundPlayer.playSound(FeedbackSoundPlayer.SOUND_BTN);
-		}
-	}
+    private void audioFeedback() {
+        if (mAppState.detectAudioFeedbackButtonActive()) {
+            FeedbackSoundPlayer.playSound(FeedbackSoundPlayer.SOUND_BTN);
+        }
+    }
 
-	private void tactileFeedback() {
-		if (mAppState.detectTactileFeedbackActive()) {
-			Vibrator vibrator = (Vibrator) this.getActivity().getSystemService(
-					Context.VIBRATOR_SERVICE);
-			vibrator.vibrate(100);
-		}
-	}
+    private void tactileFeedback() {
+        if (mAppState.detectTactileFeedbackActive()) {
+            Vibrator vibrator = (Vibrator) this.getActivity().getSystemService(
+                    Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(100);
+        }
+    }
 
-	class FeedbackClickListener implements OnPreferenceClickListener {
+    class FeedbackClickListener implements OnPreferenceClickListener {
 
-		@Override
-		public boolean onPreferenceClick(Preference preference) {
-			tactileFeedback();
-			audioFeedback();
-			return true;
-		}
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            tactileFeedback();
+            audioFeedback();
+            return true;
+        }
+    }
 
-	}
+    class FeedbackPreferenceChangeListener implements
+            OnPreferenceChangeListener {
 
-	class FeedbackPreferenceChangeListener implements
-			OnPreferenceChangeListener {
-
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object newValue) {
-			tactileFeedback();
-			audioFeedback();
-			return true;
-		}
-	}
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            tactileFeedback();
+            audioFeedback();
+            return true;
+        }
+    }
 }
